fdef17f56dc2cdba2f592f8e2b351de1befffc9e
MohamedAbdElwahabOka <boka22986@gmail.com>
2024-08-21 17:11:32 +0300

    chore: Update express import to use require instead of import

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-import express from "express";
+const express = require("express")
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World!ggggggggg");
+}
+);
+app.listen(5000, () => {
+    console.log("App is running will ");
+})
